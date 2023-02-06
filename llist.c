e6129f3db8e968a85cb728e7adf9028847023ce5
Micah Stalberg <stalberm@oregonstate.edu>
2023-02-06 13:16:43 -0800

    Adds node_alloc function

--- llist.c
+++ llist.c
@@ -1,6 +1,7 @@
 #include "llist.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 int main(int argc, char *argv[]) {
@@ -10,6 +11,16 @@ int main(int argc, char *argv[]) {
     for (int i =1; i < argc; i++) {
         char *command = argv[i];
         printf("%s\n", command);
+        struct node *n = node_alloc(10);
+        head = n;
+        printf("%d\n", head->value);
     }
+    return 0;
+}
 
+struct node *node_alloc(int value) {
+    struct node *new_node = malloc(sizeof(struct node));
+    new_node->value = value;
+    new_node->next = NULL;
+    return new_node;
 }
