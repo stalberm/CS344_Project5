4d47f7e70dd079f057052a88a0dededcf6bc12aa
Micah Stalberg <stalberm@oregonstate.edu>
2023-02-06 13:50:31 -0800

    Adds llist_delete_head method

--- llist.c
+++ llist.c
@@ -12,7 +12,9 @@ int main(int argc, char *argv[]) {
         char *command = argv[i];
         struct node *n = node_alloc(10);
         llist_insert_head(&head, n);
-        printf("%d\n", head->value);
+        printf("Old head value: %d\n", head->value);
+        struct node *old = llist_delete_head(&head);
+        printf("Deleted nodes value: %d\n", old->value);
     }
     return 0;
 }
@@ -32,4 +34,14 @@ void llist_insert_head(struct node **head, struct node *n) {
     struct node *prev_head = *head;
     *head = n;
     n->next = prev_head;
+}
+
+struct node *llist_delete_head(struct node **head) {
+    if (*head == NULL) {
+        return NULL;
+    }
+    struct node *prev_head = *head;
+    struct node *new_head = prev_head->next;
+    *head = new_head;
+    return prev_head;
 }
